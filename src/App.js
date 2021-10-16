ed03018fa369f259eafbbf70a382f05d4bd28a0c
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-10-16 21:39:44 +0100

    Add BookShelf component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 // import * as BooksAPI from './BooksAPI';
-import Book from "./Book";
-import './App.css'
+import BookShelf from "./BookShelf";
+import './App.css';
 
 export class BooksApp extends Component {
   state= {
+    categories: ["Currently Reading", "Want to Read", "Read"],
     books: [
       {
       imageUrl: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
@@ -15,19 +16,19 @@ export class BooksApp extends Component {
       {
       imageUrl: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
       category: ["Move To...","Currently Reading","Want To Read","Read","None"],
-      title: "To Kill a Mockingbird",
+      title: "Yet another title",
       author: "Harper Lee",
     },
       {
       imageUrl: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
       category: ["Move To...","Currently Reading","Want To Read","Read","None"],
-      title: "To Kill a Mockingbird",
+      title: "Another title",
       author: "Harper Lee",
       }
-    ]
+    ],
   }
   render() {
-    const books = this.state.books;
+    const {categories, books} = this.state;
     return (
       <div className="app">
         <div className="list-books">
@@ -35,13 +36,11 @@ export class BooksApp extends Component {
             <h1>MyReads</h1>
           </div>
           <div className="list-books-content">
-            <div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                  {<Book books={books}/>}
+            {categories.map((category)=> (
+              <div key={category}>
+                <BookShelf category={category} categories={categories} books={books}/>
               </div>
-              
-            </div>
+            ))}      
           </div>
         </div>
       </div>
---
+++ src/BookShelf.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Book from "./Book";
+
+export class BookShelf extends Component {
+  render() {
+    const {category, books} = this.props;
+    return (
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{category}</h2>
+            {<Book books={books}/>}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BookShelf;
